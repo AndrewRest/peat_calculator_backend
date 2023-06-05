0095543da7c900b90bc5b65154fd4d3f78c0b0e8
PavelVoitov <voitov.pavel@yandex.ru>
2023-06-05 21:34:37 +0300

    fix: local port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import {boxesRouter} from "./routes/boxes-router";
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3009
 
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
